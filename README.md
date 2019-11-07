54f097fd32d78dcf0130f84e303750b7386b243a
Jayaraj Sivadasan <jayaraj.sivadasan@gmail.com>
2019-11-06 21:06:14 -0500

    Expanding the CampDto with additional properties related to location and registering mapping profile

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 Payload is a contrract with users
 Likely want to filter data for security
 Use surrogate keys
+
+
+## Automapper feature
+
+Automapper maps by the name by default. If you have a related object such as Location  with the followng property
+
+`        public string VenueName { get; set; }`
+
+you can include this in the Dto with the following name so that automapper will automatically map it. If you dont like this method then you have to explicitly map them in the profile.
+
+`        public string LocationVenueName { get; set; }`
--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -3,12 +3,20 @@ using CoreCodeCamp.DTOs;
 
 namespace CoreCodeCamp.Data
 {
-    public class CampProfile: Profile
+    public class CampProfile : Profile
     {
 
         public CampProfile()
         {
-            this.CreateMap<Camp,CampDto>();
+            this.CreateMap<Camp, CampDto>()
+            .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
+            .ForMember(c => c.Address1, o => o.MapFrom(m => m.Location.Address1))
+            .ForMember(c => c.Address2, o => o.MapFrom(m => m.Location.Address2))
+            .ForMember(c => c.Address3, o => o.MapFrom(m => m.Location.Address3))
+            .ForMember(c => c.CityTown, o => o.MapFrom(m => m.Location.CityTown))
+            .ForMember(c => c.StateProvince, o => o.MapFrom(m => m.Location.StateProvince))
+            .ForMember(c => c.PostalCode, o => o.MapFrom(m => m.Location.PostalCode))
+            .ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country));
         }
     }
 
