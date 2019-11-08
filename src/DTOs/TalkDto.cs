6b9582cd0d70dbc6bb430d377447657aa0fe8e6d
Jayaraj Sivadasan <jayaraj.sivadasan@gmail.com>
2019-11-07 21:29:13 -0500

    Added Validations

--- src/DTOs/TalkDto.cs
+++ src/DTOs/TalkDto.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CoreCodeCamp.DTOs
 {
     public class TalkDto
     {
         public int TalkId { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string Title { get; set; }
+        [StringLength(4000, MinimumLength= 20)]
         public string Abstract { get; set; }
+        [Range(100,300)]
         public int Level { get; set; }
 
         public SpeakerDto Speaker { get; set; }
